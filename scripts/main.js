fc3d79e3653ab420fb1498304a4f8d49009ea553
MerlinDeadleft <34375235+MerlinDeadleft@users.noreply.github.com>
2026-06-02 02:43:52 +0200

    Display message when clicking login link

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 let signUpForm = document.querySelector("#sign-up-form");
+let loginButton = document.querySelector("#login-button");
 
 //main
 signUpForm.addEventListener("submit", e => {
@@ -6,4 +7,9 @@ signUpForm.addEventListener("submit", e => {
     alert("Thank you for trying to sign up!\nUnfortunately this is not a real service and your request will not be fullfilled!");
 
     return false;
+});
+
+loginButton.addEventListener("click", e => {
+    e.preventDefault();
+    alert("This is not a real service!\nTherefore there is no login page!");
 });
